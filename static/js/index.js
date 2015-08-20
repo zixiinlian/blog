279dad6a04b9d9d547f61189840486be36fb1618
zixiinlian <835898749@qq.com>
2015-08-20 14:37:59 +0800

    add category

--- static/js/index.js
+++ static/js/index.js
@@ -94,8 +94,8 @@ function categoryDisplay() {
     $('.categories-list-item').click(function() {
         var cate = $(this).attr('cate'); //get category's name
 
-        $('.post-list-body>div[post-cate!=' + cate + ']').hide(250);
-        $('.post-list-body>div[post-cate=' + cate + ']').show(400);
+        $('.post-list-body>div[post-cate!=' + cate + ']').hide();
+        $('.post-list-body>div[post-cate=' + cate + ']').show();
     });
 }
 
