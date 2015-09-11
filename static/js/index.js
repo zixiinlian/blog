05c9bc52203f1284ca9c7810b898bc304c6e5b91
zixiinlian <835898749@qq.com>
2015-09-11 13:11:17 +0800

    change style

--- static/js/index.js
+++ static/js/index.js
@@ -8,77 +8,6 @@ $(document).ready(function() {
     backToTop();
 });
 
-/**
- * load方法，页面的加载完成后触发
- * {fixFooterInit();} 固定Footer栏
- */
-/*$(window).load(function() {
-    fixFooterInit();
-});*/
-
-
-/**
- * 固定底栏的初始化方法
- * 在一开始载入页面时，使用fixFooter()方法固定底栏。
- * 在浏览器窗口改变大小是，依然固定底栏
- * @return {[type]} [description]
- */
-function fixFooterInit() {
-    var footerHeight = $('footer').outerHeight();
-    var footerMarginTop = getFooterMarginTop() - 0; //类型转换
-    // var footerMarginTop = 80;
-
-    fixFooter(footerHeight, footerMarginTop); //fix footer at the beginning
-
-    $(window).resize(function() { //when resize window, footer can auto get the postion
-        fixFooter(footerHeight, footerMarginTop);
-    });
-
-    /*    $('body').click(function() {
-        fixFooter(footerHeight, footerMarginTop);
-    });*/
-
-
-}
-
-/**
- * 固定底栏
- * @param  {number} footerHeight    底栏高度
- * @param  {number} footerMarginTop 底栏MarginTop
- * @return {[type]}                 [description]
- */
-function fixFooter(footerHeight, footerMarginTop) {
-    var windowHeight = $(window).height();
-    var contentHeight = $('body>.container').outerHeight() + $('body>.container').offset().top + footerHeight + footerMarginTop;
-    // console.log("window---"+windowHeight);
-    // console.log("$('body>.container').outerHeight()---"+$('body>.container').outerHeight() );
-    // console.log("$('body>.container').height()---"+$('body>.container').height() );
-    // console.log("$('#main').height()--------"+$('#main').height());
-    // console.log("$('body').height()--------"+$('body').height());
-    //console.log("$('#main').html()--------"+$('#main').html());
-    // console.log("$('body>.container').offset().top----"+$('body>.container').offset().top);
-    // console.log("footerHeight---"+footerHeight);
-    // console.log("footerMarginTop---"+footerMarginTop);
-    if (contentHeight < windowHeight) {
-        $('footer').addClass('navbar-fixed-bottom');
-    } else {
-        $('footer').removeClass('navbar-fixed-bottom');
-    }
-
-    $('footer').show(400);
-}
-
-/**
- * 使用正则表达式得到底栏的MarginTop
- * @return {string} 底栏的MarginTop
- */
-function getFooterMarginTop() {
-    var margintop = $('footer').css('marginTop');
-    var patt = new RegExp("[0-9]*");
-    var re = patt.exec(margintop);
-    // console.log(re[0]);
-    return re[0];
-}
 
 /**
  * 分类展示
@@ -93,9 +22,11 @@ function categoryDisplay() {
 
         if(cate === 'All'){
             $('.post-list-body article').show();
+            location.hash = '';
         }else{
             $('.post-list-body>article[data-category!=' + cate + ']').hide();
             $('.post-list-body>article[data-category=' + cate + ']').show();
+            location.hash = cate;
         }
     });
 }
